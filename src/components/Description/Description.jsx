e4ac5197a3e39372f44e28a8a224ca840c1273a9
ale-genovesio <ale.genovesio1999@gmail.com>
2024-04-05 19:57:19 +0200

    add description component

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import Text from "../Text/Text";
+import messages from "../../messages/messages.json";
+import "./description.css"
+
+const Description = () => {
+    return (
+        <div className="description">
+            <h1 className="desc-left"><Text message={messages?.description?.["title-description-description"]} /></h1>
+            <div className="desc-right">
+                <Text message={messages?.description?.["text-description-description"]} />
+                {/* SISTEMARE IL LINK NELLA PAGINA GIUSTA */}
+                <a href="Homepage">Scopri di piu'</a>
+
+            </div>
+        </div>
+    )
+};
+
+export default Description;
