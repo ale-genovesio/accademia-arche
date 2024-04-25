bc68f7994c1cf90f38e746780c523d81a5d3c88d
ale-genovesio <ale.genovesio1999@gmail.com>
2024-04-25 20:22:52 +0200

    delete main

--- src/components/Item/Item.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./item.css"
-
-const Item = ({ message, classNameItem }) => {
-  return <div className={classNameItem}>{message}</div>;
-};
-
-export default Item;
